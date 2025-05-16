ae34fc277953a5f0eb18a997613952f7cfec3a15
BryanHong <bryan774356@gmail.com>
2025-05-16 10:40:55 +0800

    加入 vercel Speed Insights 功能

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist } from "next/font/google";
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const geistSans = Geist({
   subsets: ["latin"],
@@ -20,6 +21,7 @@ export default function RootLayout({
     <html lang="zh-TW">
       <body className={`${geistSans.className} antialiased`}>
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
